3a3045b5a95c5aafcf5c9d108d5cc1ace4e7b23f
Jeff Rohalla <jrohalla@uchicago.edu>
2015-01-24 16:08:38 -0600

    Working Serving Existing Files

--- Server.java
+++ Server.java
@@ -4,13 +4,24 @@ import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.logging.SocketHandler;
 
 public final class Server {
 	private final int serverPort;
 	private ServerSocket socket;
+	private Socket mClientSocket;
 	private DataOutputStream toClientStream;
 	private BufferedReader fromClientStream;
 
+	public void serverCleanup(){
+		try {
+			fromClientStream.close();
+			toClientStream.close();
+			mClientSocket.close();
+		} catch (IOException e){
+			System.out.println(e);
+		}
+	}
 	public static void main(String argv[]) {
 		Map<String, String> flags = Utils.parseCmdlineFlags(argv);
 		if (!flags.containsKey("--serverPort")) {
@@ -27,22 +38,26 @@ public final class Server {
 		}
 
 		Server server = new Server(serverPort);
-		try {
-			server.bind();
-			if (server.acceptFromClient()) {
-				// Parse Header
-				ArrayList<String> x = server.getRequestHeader();
-				String[] requests = x.get(0).split(" ");
-				server.processRequest(requests[0], requests[1]);
-				//Look for file in filesystem
+		server.bind();
+		while(true) {
+			try{
+				if (server.acceptFromClient()) {
+					// Parse Header
+					ArrayList<String> x = server.getRequestHeader();
+					String[] requests = x.get(0).split(" ");
+					server.processRequest(requests[0], requests[1]);
+					//Look for file in filesystem
 //				server.serveFile(requests[1]);
-				// Build response header
-				// send response
-			} else {
-				System.out.println("Error accepting client connection.");
+					// Build response header
+					// send response
+				} else {
+					System.out.println("Error accepting client connection.");
+				}
+			} catch (IOException e) {
+				System.out.println("Error communicating with client. aborting. Details: " + e);
 			}
-		} catch (IOException e) {
-			System.out.println("Error communicating with client. aborting. Details: " + e);
+
+			server.serverCleanup();
 		}
 	}
 
@@ -55,22 +70,25 @@ public final class Server {
 	 *
 	 * @throws {@link IOException} if the port is already in use.
 	 */
-	public void bind() throws IOException {
-		socket = new ServerSocket(serverPort);
-		System.out.println("Server bound and listening to port " + serverPort);
+	public void bind()  {
+		try{
+			socket = new ServerSocket(serverPort);
+			System.out.println("Server bound and listening to port " + serverPort);
+		} catch (IOException e) {
+			System.out.println("Error binding to port " + serverPort);
+		}
 	}
 
 	/**
 	 * Waits for a client to connect, and then sets up stream objects for communication
- 	 * in both directions.
+	 * in both directions.
 	 *
 	 * @return {@code true} if the connection is successfully established.
 	 * @throws {@link IOException} if the server fails to accept the connection.
 	 */
 	public boolean acceptFromClient() throws IOException {
-		Socket clientSocket;
 		try {
-			clientSocket = socket.accept();
+			mClientSocket = socket.accept();
 		} catch (SecurityException e) {
 			System.out.println("The security manager intervened; your config is very wrong. " + e);
 			return false;
@@ -79,23 +97,10 @@ public final class Server {
 			return false;
 		}
 
-		toClientStream = new DataOutputStream(clientSocket.getOutputStream());
-		fromClientStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		return true;
-	}
 
-	/**
-	 * Loops forever, reading a line from the client, printing it to the screen,
-	 * then echoing it back.
-	 *
-	 * @throws (@link IOException} if a communication error occurs.
-	 */
-	public void echoLoop() throws IOException {
-		while (true) {
-			String blob = fromClientStream.readLine();
-			System.out.println(blob);
-			toClientStream.writeBytes(blob + '\n');
-		}
+		toClientStream = new DataOutputStream(mClientSocket.getOutputStream());
+		fromClientStream = new BufferedReader(new InputStreamReader(mClientSocket.getInputStream()));
+		return true;
 	}
 
 	public ArrayList<String> getRequestHeader () throws IOException {
@@ -146,28 +151,51 @@ public final class Server {
 
 	// http://stackoverflow.com/questions/7707555/getting-date-in-http-format-in-java
 	public String getServerDate(){
-			Calendar calendar = Calendar.getInstance();
-			SimpleDateFormat dateFormat = new SimpleDateFormat(
-					"EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
-			dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-			return dateFormat.format(calendar.getTime());
+		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat dateFormat = new SimpleDateFormat(
+				"EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+		return dateFormat.format(calendar.getTime());
+	}
+
+	public void sendResponse(String header, File file){
+
+		try {
+			toClientStream.writeBytes(header);
+			if (file != null) {
+				byte[] buffer = new byte[1000];
+				FileInputStream in = new FileInputStream(file);
+				while (in.available() > 0) {
+					toClientStream.write(buffer, 0, in.read(buffer));
+				}
+			}
+//			toClientStream.writeBytes(content);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	public void get(String resourcePath){
 		System.out.println("laskjdglakdsj" + resourcePath);
 		File resource = new File("www" + resourcePath);
-		String header = buildHeader(200, "OK", "text/html", resource.length());
+		if (resource.exists()){
+			String header = buildHeader(200, "OK", "text/html", resource.length());
 
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("www" + resourcePath));
-		String line = null;
-		while ((line = br.readLine()) != null) {
-			header += line;
+//			try {
+//				BufferedReader br = new BufferedReader(new FileReader("www" + resourcePath));
+//				String line = null;
+//				while ((line = br.readLine()) != null) {
+//					header += line;
+//				}
+				sendResponse(header, resource);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+		} else {
+			// TODO: Look in defs file for redirect
+			String header = buildHeader(404, "Not Found", "text/html", 0 );
+			sendResponse(header, null);
 		}
 
-			toClientStream.writeBytes(header);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	public void head(String resourcePath){
