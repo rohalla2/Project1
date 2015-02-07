881ed4fd7db0ddfa6b8a8a73236b01a0ec368e31
Jeff Rohalla <jrohalla@uchicago.edu>
2015-02-07 12:28:43 -0600

    Update tests to be more robust

--- test_client.py
+++ test_client.py
@@ -3,12 +3,13 @@
 import sys
 import time
 import urllib2
+import os
 from urllib2 import urlopen,HTTPError
 
 TIMEOUT = 5
-NORMAL_FILES = ['/index.html', '/foo/bar.html']
+NORMAL_FILES = ['/index.html', '/foo/bar.html', '/test.pdf', '/test.txt', '/test.png', '/test.jpg']
 REDIRECTS = [['/cats', 'http://en.wikipedia.org/wiki/Cat']]
-NOTFOUNDS = ['/redirect.defs', '/not/a/real/url.html']
+NOTFOUNDS = ['/redirect.defs', '/not/a/real/url.html', '/']
 
 # prevents us from following redirects, so we can get the real response code
 class DontFollowHttpRedirectHandler(urllib2.HTTPRedirectHandler):
@@ -16,6 +17,17 @@ class DontFollowHttpRedirectHandler(urllib2.HTTPRedirectHandler):
 		raise urllib2.HTTPError(req.get_full_url(), code, msg, headers, fp)
 	http_error_302 = http_error_303 = http_error_307 #= http_error_301
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    test_fail = FAIL + 'FAIL'  + ENDC
+    test_pass = OKGREEN + 'PASS' + ENDC
 
 def build_url(host, port, filename):
 	return 'http://' + host + ':' + port + filename
@@ -38,16 +50,16 @@ def test_POST(host, port):
 	for filename in NORMAL_FILES:
 		request, response, code = maybe_fetch(host, port, filename, op='POST')
 		if code != 403:
-			return 'FAIL'
-	return 'PASS'
+			return bcolors.test_fail
+	return bcolors.test_pass
 
 def test_INVALID(host, port):
 	print 'test INVALID'
 	for filename in NORMAL_FILES:
 		request, response, code = maybe_fetch(host, port, filename, op='FOOBAR')
 		if code != 403 and code < 500:
-			return 'FAIL'
-	return 'PASS'
+			return bcolors.test_fail
+	return bcolors.test_pass
 
 def test_200(host, port, opcode='GET'):
 	print 'test 200s ' + opcode
@@ -56,10 +68,10 @@ def test_200(host, port, opcode='GET'):
 		print '\t%s' % (filename)
 		request, response, code = maybe_fetch(host, port, filename, op=opcode)
 		if code != 200 or response is None:
-			return 'FAIL'
+			return bcolors.test_fail
 		if opcode=='HEAD' and len(response.readlines()) != 0:
-			return 'FAIL'
-	return 'PASS'
+			return bcolors.test_fail
+	return bcolors.test_pass
 
 def test_404(host, port):
 	print 'test 404s'
@@ -67,8 +79,8 @@ def test_404(host, port):
 		print '\t%s' % (filename)
 		request, response, code = maybe_fetch(host, port, filename)
 		if code != 404 or response != None:
-			return 'FAIL'
-	return 'PASS'
+			return bcolors.test_fail
+	return bcolors.test_pass
 
 def test_301(host, port, opcode='GET'):
 	print 'test 301s ' + opcode
@@ -79,11 +91,40 @@ def test_301(host, port, opcode='GET'):
 		# because we followed the redirect, the code should actually be 200,
 		# and there should be a response.
 		if code != 200 or response == None:
-			return 'FAIL'
+			return bcolors.test_fail
 		if response.url != redirect:
-			return 'FAIL'
-	return 'PASS'
-	
+			return bcolors.test_fail
+	return bcolors.test_pass
+
+def expected_response_type(fileExtension):
+	if fileExtension == '.html':
+		return 'text/html'
+	elif fileExtension == '.txt':
+		return 'text/plain'
+	elif fileExtension == '.pdf':
+		return 'application/pdf'
+	elif fileExtension == '.png':
+		return 'image/png'
+	elif fileExtension in {'.jpeg', '.jpg'}:
+		return 'image/jpeg'
+	else:
+		return ''
+
+def test_response_type(host, port, opcode='GET'):
+	print 'test Valid Response Type ' + opcode
+	isPass = True
+	for filename in NORMAL_FILES:
+		request, response, code = maybe_fetch(host, port, filename, op=opcode)
+		if response.headers.type != expected_response_type(os.path.splitext(filename)[1]):
+			print '\t' + filename + ' ' + response.headers.type + ' ' + bcolors.test_fail
+			isPass = False
+		else:
+			print '\t' + filename + ' ' + response.headers.type + ' ' + bcolors.test_pass
+	if isPass:
+		return bcolors.test_pass
+	else:
+		return bcolors.test_fail
+
 def parse_flags(argv):
 	arg_map = {}
 	if len(argv) <= 1: return {}
@@ -108,3 +149,4 @@ if __name__  == '__main__':
 	print test_301(host, port, opcode='HEAD') + '\n'
 	print test_POST(host, port) + '\n'
 	print test_INVALID(host, port) + '\n'
+	print test_response_type(host, port) + '\n'
